fbaf72f30e3c985c7b85d79ea1fc11bb5b4ec71d
Axel <axel.hemissi@agena3000.com>
2021-10-04 15:34:07 +0200

    Get creator from posts | Hide email

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -4,10 +4,12 @@ import {
 	Arg,
 	Ctx,
 	Field,
+	FieldResolver,
 	Mutation,
 	ObjectType,
 	Query,
 	Resolver,
+	Root,
 } from 'type-graphql';
 import * as argon2 from 'argon2';
 import { COOKIE_NAME, FORGET_PASSWORD_PREFIX } from '../constants';
@@ -34,8 +36,17 @@ class UserResponse {
 	errors?: FieldError[];
 }
 
-@Resolver()
+@Resolver(User)
 export class UserResolver {
+	@FieldResolver(() => String)
+	email(@Root() user: User, @Ctx() { req }: MyContext) {
+		if (req.session.userId === user.id) {
+			return user.email;
+		}
+
+		return '';
+	}
+
 	@Query(() => User, { nullable: true })
 	me(@Ctx() { req }: MyContext) {
 		if (!req.session.userId) {
